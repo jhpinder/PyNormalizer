b06dc2c0a237a2fbd69d3c58d5b232a0ba0d17b5
James Pinder <jhpinder@gmail.com>
2020-07-31 12:55:07 -0400

    use soundfile instead of wavio

--- normalizer.py
+++ normalizer.py
@@ -3,20 +3,15 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import numpy as np
-import wavio as w
-import PyWave as pw
+import soundfile as sf
 
 Tk().withdraw()
 filename = askopenfilename()
-#wf = w.read(filename)
-wf = pw.open(filename)
-data = wf.read()
-fs = wf.frequency
-print(data)
+data, fs = sf.read(filename)
 maxL = max(np.abs(data[:,0]))
 maxR = max(np.abs(data[:,1]))
 maxVal = max(maxL, maxR)
 data = data / maxVal
 
-w.write(filename.split('.')[0] + "_normalized.wav", data, fs, sampwidth=3)
+sf.write(filename.split('.')[0] + "_normalized.wav", data, fs)
 print(filename)
